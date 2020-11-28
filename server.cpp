f45ebf5704cc49d462824d5ff7713c70a1eb2e47
hqureshi434 <hqureshi434@gmail.com>
2020-11-28 13:31:29 -0800

    Added in the pseudocode from the assignment PDF into the cpp files.

---
+++ server.cpp
@@ -0,0 +1,37 @@
+//Pseudocode (with changes) from the assignment PDF for the server
+/*
+//Server code
+ from socket import ∗
+
+ //The port on which to listen
+ serverPort = 12000
+
+ //Create a TCP socket
+ serverSocket = socket(AF INET, SOCK STREAM) 9
+ //Bind the socket to the port
+ serverSocket.bind(( ’ ’, serverPort))
+
+ //Start listening for incoming connections
+ serverSocket.listen(1)15
+ print ”The server is ready to receive” 17
+ //Forever accept incoming connections
+ while 1 :
+ //Accept a connection ; get client’s socket
+ connection Socket , addr = serverSocket.accept()
+
+ //The temporary buffer
+ tmpBuff= ”” 25
+ while len (data) != 40:
+ //Receive whatever the newly connected client has to send
+ tmpBuff = connectionSocket . recv (40 29
+ //The other side unexpectedly closed it’s socket
+ if not tmpBuff :
+ break
+
+ //Save the data
+ data += tmpBuff
+
+ print data 38
+ //Close the s ocket
+  connection Socket.close()
+  */
